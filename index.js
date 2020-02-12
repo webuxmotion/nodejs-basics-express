8c309ceca58f6e7d0418b006c789e626d42e53d1
Pereverziev Andrii <pereverziev.andrii@gmail.com>
2020-02-12 18:24:36 +0200

    Work with html files

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express')
+const path = require('path')
 const app = express()
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
